fc84c86801fdeb3a81e35a3c561c22bc4dba7a43
嘉泽 孙 <1249032862@qq.com>
2024-12-04 16:40:13 +0800

    生成上三角矩阵的钻石量

--- generate_mine.py
+++ generate_mine.py
@@ -15,8 +15,10 @@ def generate_mine_field(layers: int):
     mine_field = np.zeros((layers, layers), dtype=int)
 
     # 为每个位置生成一个矿产值
-    for i in range(layers):
-        for j in range(layers-i):  # 只在左上三角区域内生成矿产
-            mine_field[i, j] = np.random.normal(loc=50, scale=20)
+    for i in range(0, layers):
+        for j in range(0, layers - i):  # 只在左上三角区域内生成矿产
+            mine_field[i, j] = np.random.normal(loc=50, scale=10)
+
+    print(mine_field)
 
     return mine_field
